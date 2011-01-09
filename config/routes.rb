aed7097ddc7db7f9eccb8e20634c49b0868f0b57
Hani Elabed <hani.elabed@gmail.com>
2011-01-09 04:01:35 -0600

    removed index.html added a root route

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,8 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  #root :to => "welcome#index"
+  root :to => "say#hello"
 
   # See how all your routes lay out with "rake routes"
 
